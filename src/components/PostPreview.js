8d06f6d2d92cba153729a1bce9b7f97fe0c294be
Neetish Raj <neetishop@gmail.com>
2022-07-12 20:59:13 +0530

    Completed the React Loader assignement

---
+++ src/components/PostPreview.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {  NavLink, useParams } from 'react-router-dom';
+
+const PostPreview = ({ post }) => {
+    
+    return (
+        <div className="post-preview">
+            <h2 className="post-title">
+                <NavLink to={`post/${post.id}`}>{post.title}</NavLink>
+            </h2>
+        </div>
+
+    )
+}
+
+
+export default PostPreview;
